6639e828290ca857fad90cf8d04bf839be49db0c
ayoub_jouini <ayoubjouini135@gmail.com>
2021-07-30 05:00:24 +0100

    navBar DropDown

---
+++ src/components/nav-bar/NavBarDropdown.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const NavBarDropdown = ({ dropDown }) => {
+    return (
+        <ul className="NAVBAR-MENU-DROPDOWN dropdown-menu" aria-labelledby="navbarDropdown">
+            {dropDown.map((d, key) => {
+                return (
+                    <Link to={`/${d}`} key={key} className="NAVBAR-LINK">
+                        <li><a className="dropdown-item" href="#">{d}</a></li>
+                    </Link>
+                )
+            })}
+
+        </ul>
+    );
+}
+
+export default NavBarDropdown;

6c088d5e350852dfba76233627407421441a75ba
ayoub_jouini <ayoubjouini135@gmail.com>
2021-07-30 23:46:33 +0100

    imgSlide

--- src/components/images-slide/img-slide.js
+++ src/components/images-slide/img-slide.js
@@ -8,28 +8,38 @@ const IMGSlide = () => {
     if (error) throw error;
     if (loading) return <LoadingSpinner />
     return (
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="IMGSlid-div carousel-inner">
-                <div class="carousel-item active">
-                    <img src="./asset/images/felipe-galvan-AhfrA5VQNpM-unsplash.jpg" class="d-block w-100" alt="felipe-galvan-AhfrA5VQNpM-unsplash" />
-                </div>
-                <div class="carousel-item">
-                    <img src="./asset/images/hannah-morgan-ycVFts5Ma4s-unsplash.jpg" class="d-block w-100" alt="hannah-morgan-ycVFts5Ma4s-unsplash" />
-                </div>
-                <div class="carousel-item">
-                    <img src="./asset/images/keagan-henman-xPJYL0l5Ii8-unsplash.jpg" class="d-block w-100" alt="keagan-henman-xPJYL0l5Ii8-unsplash" />
-                </div>
-                <div class="carousel-item">
-                    <img src="./asset/images/nimble-made-DRM_6zFkPFw-unsplash.jpg" class="d-block w-100" alt="nimble-made-DRM_6zFkPFw-unsplash" />
-                </div>
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+            <div className="IMGSlid-div carousel-inner">
+                {data && data.length > 0 && data.map(d => {
+                    return (
+                        d.imgslid.map((img, key) => {
+
+                            if (key === 0) {
+                                return (
+                                    <div key={key} className='carousel-item active'>
+                                        <img src={`./asset/images/${img}`} className="d-block w-100" alt={img} />
+                                    </div>
+                                );
+                            }
+                            else {
+                                return (
+                                    <div key={key} className='carousel-item'>
+                                        <img src={`./asset/images/${img}`} className="d-block w-100" alt={img} />
+                                    </div>
+                                );
+                            }
+                        })
+                    )
+                })}
+
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
     );

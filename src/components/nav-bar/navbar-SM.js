b2ec18b9ec16d068b3b4f8de182a007a726ee8d3
ayoub_jouini <ayoubjouini135@gmail.com>
2021-07-30 05:12:14 +0100

    navbar-SM

---
+++ src/components/nav-bar/navbar-SM.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBarSM = () => {
+    return (
+        <div className="NAVBAR-SM">
+            <img src="./asset/icons/facebook.svg" />
+            <img src="./asset/icons/instagram.svg" />
+            <img src="./asset/icons/twitter.svg" />
+            <img src="./asset/icons/youtube.svg" />
+            <img src="./asset/icons/whatsapp.svg" />
+        </div>
+    );
+}
+
+export default NavBarSM;

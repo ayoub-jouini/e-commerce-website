31d3f61211cb88cfccb6cef2634c049beefcdac2
ayoub_jouini <ayoubjouini135@gmail.com>
2021-08-22 21:26:49 +0100

    server

---
+++ Server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const productRoute = require('./routes/product-routes');
+
+app.use('/api/products', productRoute);
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'an unknown error occured!' });
+});
+
+app.listen(5000, () => {
+    console.log("listen: port 5000")
+})
